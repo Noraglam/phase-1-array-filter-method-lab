c9729fdcf112905cb85f38ec8a6b06f63e423b1e
Nora <noraonyi@gmail.com>
2022-04-10 22:25:44 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+const drivers =['Bobby','Sammy', 'Sally','Annette','Sarah','bobby']
+function findMatching(arr,string) {
+    let newArray =[]
+    for(let driver of arr) {
+        if (driver === string) {
+    
+            newArray.push (driver)  
+      }
+    }
+     return newArray
+}
+
+console.log (findMatchingdrivers(drivers,"Bobby"))
+
+     
+      function fuzzyMatch(array, string){
+    let newArray = array.filter(element => {
+        //debugger
+        return element[0] ==="S"
+
+    })
+    return newArray
+}
+
+fuzzyMatch(drivers, "Sa")
+
+    
+      
+
+    
